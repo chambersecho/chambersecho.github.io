b84068312c4ea61336781f68e9892792a94c4a31
Yin Wrong <contact@yinwrong.com>
2025-10-15 12:57:08 +0100

    updated profile pic

--- src/components/pages/Contact.tsx
+++ src/components/pages/Contact.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import './Contact.css';
-import yinwoods_head from '../assets/images/yinwoods_head.jpeg';
+import yw_profilepic_04 from '../assets/ProfielPic/yw_profilepic_04.jpeg';
 import { Link } from "react-router-dom";
 
 export const Contact = () => {
@@ -10,7 +10,7 @@ export const Contact = () => {
     return (
       <main className="contact">
         <div className="desc">
-          <img alt='' src={yinwoods_head}></img>
+          <img alt='' src={yw_profilepic_04}></img>
           <Link to="/">Yin Wrong</Link> is a Hong Kong-born, London-based artist exploring themes of existentialism, time, and identity through frame-by-frame 2D animation and willow charcoal painting. Their work draws from the aesthetic of memento mori, the persistence of Norman McLaren, and the spirit of resistance embodied in the Harlem Renaissance. Yin's practice navigates the tension between memory, ephemerality, and endurance—asking what it means to exist, to vanish, and to leave a trace.
           <div >
             <br/>
