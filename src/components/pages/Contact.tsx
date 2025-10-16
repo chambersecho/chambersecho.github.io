63ae6b834eb0dcc4c87c443d5d9e87e56640f01a
Yin Wrong <contact@yinwrong.com>
2025-10-16 01:10:25 +0100

    updated bio

--- src/components/pages/Contact.tsx
+++ src/components/pages/Contact.tsx
@@ -11,12 +11,12 @@ export const Contact = () => {
       <main className="contact">
         <div className="desc">
           <img alt='' src={yw_profilepic_03}></img>
-          <Link to="/">Yin Wrong</Link> is a Hong Kong-born, London-based artist exploring themes of existentialism, time, and identity through frame-by-frame 2D animation and willow charcoal painting. Their work draws from the aesthetic of memento mori, the persistence of Norman McLaren, and the spirit of resistance embodied in the Harlem Renaissance. They explore the relationship between Evanescence, Momentariness, and Transience. They constantly ask what is “real” amidst delirium and chaos, and wonder what would be the last shape and shade an individual sees when the robots of re-education come to wipe our memories. 
+          <Link to="/">Yin Wrong</Link> is a Hong Kong-born, London-based artist exploring themes of existentialism, time, and identity through frame-by-frame 2D animation and willow charcoal painting. Their work draws from the aesthetic of memento mori, the persistence of Norman McLaren, and the spirit of resistance embodied in the Harlem Renaissance. They explore the relationship between Evanescence, Momentariness, and Transience. They constantly ask what is “real” amidst delirium and chaos, and wonder what would be the last picture an individual sees when the robots of re-education come to wipe our memories. 
 
 
           <div >
             <br/>
-            Fully autodidact and raised in an indifferent and unsupportive environment, their work is built on creative rebellion, and their very being is a testament to self-reliance.
+            Fully autodidact and raised in an indifferent and unsupportive environment, their work is built on creative rebellion, <br></br>and their very being is a testament to self-reliance.
             <br/>
             <br/>
             Please, no junk mail. contact@yinwrong.com <br></br>Any email related to my career as an artist would be cc to @edulaweb@gmail.com <br></br>Simon Edwards - my favourite manager in the world. 
