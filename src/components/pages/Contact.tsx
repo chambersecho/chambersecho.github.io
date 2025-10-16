dd561cbb4f7069f1127eb6b25e8f333cd3dd0494
Yin Wrong <contact@yinwrong.com>
2025-10-16 13:49:36 +0100

    updated bio

--- src/components/pages/Contact.tsx
+++ src/components/pages/Contact.tsx
@@ -14,7 +14,7 @@ export const Contact = () => {
           <Link to="/">Yin Wrong</Link> is a Hong Kong-born, London-based artist exploring themes of existentialism, time, and identity through frame-by-frame animation and willow charcoal painting. Their work draws from the aesthetic of <Link to="/">Memento Mori</Link>, the persistence of <Link to="/">Norman McLaren</Link>, and the spirit of resistance embodied in the <Link to="/">Harlem Renaissance</Link>. They explore the relationship between Ephemerality, Momentariness, and Timelessness. They constantly ask what is “real” amidst delirium and chaos, and wonder what would be the last picture of Evanescence we see when the robots of re-education come to wipe our memories. 
 <div >
             <br/>
-            Fully autodidact and raised in an indifferent and unsupportive environment, their work is built on creative rebellion, and their very being is a testament to self-reliance.
+            Fully autodidact, raised in an indifferent and unsupportive environment, their work is built on creative rebellion, and their very being is a testament to self-reliance.
             <br/>
             <br/>
             Please, no junk mail. contact@yinwrong.com <br></br>Any email related to my career as an artist would be cc to edulaweb@gmail.com <br></br>Simon Edwards - my favourite manager in the world. 
