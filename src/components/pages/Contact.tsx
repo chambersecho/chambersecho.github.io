d33f7edca3f345c7242bfc19b3c850dd5848745d
Yin Wrong <contact@yinwrong.com>
2026-06-03 17:41:50 +0100

    updated Contact

--- src/components/pages/Contact.tsx
+++ src/components/pages/Contact.tsx
@@ -11,7 +11,7 @@ export const Contact = () => {
       <main className="contact">
         <div className="desc">
           <img alt='' src={yw_profilepic_03}></img>
-          <Link to="/">Yin Wrong</Link> is a Hong Kong-born, Autistic artist exploring themes of existentialism, time, and identity through frame-by-frame 2D animation, woodblock and stone. Their work draws from the aesthetic of memento mori, the persistence of Norman McLaren, and the spirit of resistance embodied in the Harlem Renaissance. Yin's practice navigates the tension between memory, ephemerality, and endurance—asking what it means to exist, to vanish, and to leave a trace.
+          <Link to="/">Yin Wrong</Link> is a Hong Kong-born, Autistic artist exploring themes of existentialism, time, and identity through frame-by-frame 2D animation, experiments with Marble, stones and woodcut. Their work draws from the aesthetic of memento mori, the persistence of Norman McLaren, and the spirit of resistance embodied in the Harlem Renaissance.
           <div >
             <br/>
             A fully autodidactic artist, raised in an indifferent and unsupportive environment, their work is built on creative rebellion, and their very being is a testament to self-reliance.
